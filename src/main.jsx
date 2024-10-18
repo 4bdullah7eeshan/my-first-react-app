096ae584a7f2a72a09b038f8663729dc03913a99
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 16:31:38 +0530

    feat: complete challenge 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import FeedbackForm from './react.dev/state-a-components-memory/challenge-03/FeedbackForm'
+import FeedbackForm from './react.dev/state-a-components-memory/challenge-04/FeedbackForm'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
