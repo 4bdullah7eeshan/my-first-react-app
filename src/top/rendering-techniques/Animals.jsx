e7f8fdd964005ab5abcb50b2e15315c8bab8b252
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 11:18:55 +0530

    feat: render a list of react components with props

--- src/top/rendering-techniques/Animals.jsx
+++ src/top/rendering-techniques/Animals.jsx
@@ -1,18 +1,27 @@
+function ListItem(props) {
+    return <li>{props.animal}</li>
+}
+  
+function List(props) {
+    return (
+      <ul>
+        {props.animals.map((animal) => {
+          return <ListItem key={animal} animal={animal} />;
+        })}
+      </ul>
+    );
+}
+  
 function Animals() {
     const animals = ["Lion", "Cow", "Snake", "Lizard"];
-    const animalsList = animals.map((animal) => <li key={animal}>{animal}</li>)
   
     return (
       <div>
         <h1>Animals: </h1>
-        <ul>
-          {animalsList}
-        </ul>
+        <List animals={animals} />
       </div>
     );
 }
-  
-  
 
 export default Animals;
   
