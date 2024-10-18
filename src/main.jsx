22895fffeca64e304e2e1855e501abadb8a100d9
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 09:46:53 +0530

    feat: write jsx abiding with its rules

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import FavoriteFood from './FavoriteFood'
+import Svg from './Svg'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FavoriteFood />
+    <Svg />
   </StrictMode>,
 )
