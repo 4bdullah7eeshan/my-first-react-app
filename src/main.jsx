e8d5b2ff4f1f62052e2d8e53cb4d85e0a0cd9fea
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 16:12:57 +0530

    feat: wip challenge 1, fix next button on slide 12

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Page from './react.dev/state-a-components-memory/Page'
+import Gallery from './react.dev/state-a-components-memory/challenge-01/Gallery'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Page />
+    <Gallery />
   </StrictMode>,
 )
