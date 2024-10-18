92b93027a44d67e083ee60928946bcfe9022390e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 16:22:10 +0530

    feat: complete challenge 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Gallery from './react.dev/state-a-components-memory/challenge-01/Gallery'
+import Form from './react.dev/state-a-components-memory/challenge-02/Form'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Gallery />
+    <Form />
   </StrictMode>,
 )
