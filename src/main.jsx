595e3bd08ec1e52fd83610d500b09e403bee183d
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 10:08:47 +0530

    feat: complete react.dev jsx challenge 1/1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ToDoList from './ToDoList'
+import Bio from './Bio'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ToDoList />
+    <Bio />
   </StrictMode>,
 )
