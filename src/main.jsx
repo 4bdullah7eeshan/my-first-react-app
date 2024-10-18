55fc47a404c617737dd29d7e61fb0afd0939de2c
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 14:33:45 +0530

    feat: complete challenge 4/4

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import RecipeList from './react.dev/rendering-lists/challenge-02/RecipeList'
+import Poem from './react.dev/rendering-lists/challenge-04/Poem'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RecipeList />
+    <Poem />
   </StrictMode>,
 )
