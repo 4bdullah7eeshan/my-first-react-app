847f9cbfe64cd8ef7544fe2804da89e1e0fe82ca
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 11:53:41 +0530

    feat: complete challenge 1/3

--- src/react.dev/conditional-rendering/PackingList.jsx
+++ src/react.dev/conditional-rendering/PackingList.jsx
@@ -1,39 +1,29 @@
 function Item({ name, isPacked }) {
-    let itemContent = name;
-    if (isPacked) {
-        itemContent = (
-        <del>
-            {name + "✅"}
-        </del>
-        );
-    }
     return (
-        <li className="item">
-        {itemContent}
-        </li>
+      <li className="item">
+        {isPacked ? name + ' ✅' : name + ' ❌'}
+      </li>
     );
 }
-
-function PackingList() {
+  
+export default function PackingList() {
     return (
-        <section>
-            <h1>Sally Ride&apos;s Packing List</h1>
-            <ul>
-                <Item
-                    isPacked={true}
-                    name="Space suit"
-                />
-                <Item
-                    isPacked={true}
-                    name="Helment with a golden leaf"
-                />
-                <Item
-                    isPacked={false}
-                    name="Photo of Tam"
-                />
-            </ul>
-        </section>
+      <section>
+        <h1>Sally Ride's Packing List</h1>
+        <ul>
+          <Item 
+            isPacked={true} 
+            name="Space suit" 
+          />
+          <Item 
+            isPacked={true} 
+            name="Helmet with a golden leaf" 
+          />
+          <Item 
+            isPacked={false} 
+            name="Photo of Tam" 
+          />
+        </ul>
+      </section>
     );
 }
-
-export default PackingList;
