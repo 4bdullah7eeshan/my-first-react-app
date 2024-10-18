0c69036dac8569e31458793bcfa1b46e1f68edda
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 11:03:24 +0530

    feat: use js in jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Bio from './Bio'
+import ToDoList from './react.dev/javascript-in-jsx-with-curly-braces/ToDoList'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Bio />
+    <ToDoList />
   </StrictMode>,
 )
---
+++ src/react.dev/javascript-in-jsx-with-curly-braces/ToDoList.jsx
@@ -0,0 +1,8 @@
+export default function TodoList() {
+    const name = 'Gregorio Y. Zara';
+    return (
+        <>
+            <h1>{name}&apos;s To Do List</h1>
+        </>
+    );
+}
