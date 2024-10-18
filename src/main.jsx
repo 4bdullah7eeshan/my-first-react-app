c2acc3bfc5f61abcddc1b5ab274e5deec68b20d2
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 18:11:10 +0530

    chore: move files in correct folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import FeedbackForm from './react.dev/state-as-a-snapshot/FeedbackForm'
+import FeedbackForm from './react.dev/choosing-the-state-structure/FeedbackForm'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
