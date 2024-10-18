ac09ba437038474803cdb0f0f3a60666425856e2
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 09:33:04 +0530

    feat: create and use a FavoriteFood component

---
+++ src/FavoriteFood.jsx
@@ -0,0 +1,24 @@
+function FavoriteFood() {
+    return (
+        <div>
+            <h1>Favorite Food</h1>
+            <p>I love cherishing on the following items:</p>
+            <ul>
+                <li>
+                    Cakes
+                </li>
+                <li>
+                    Chocolates
+                </li>
+                <li>
+                    Coffee
+                </li>
+                <li>
+                    Pastries
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default FavoriteFood;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Greeting from './Greeting.jsx'
+import FavoriteFood from './FavoriteFood'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting />
+    <FavoriteFood />
   </StrictMode>,
 )
