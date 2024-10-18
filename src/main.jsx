cafd9edab2dfea43d6e4c3b25874bcec3f7f6f17
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 14:39:39 +0530

    feat: try the crypto.randomUUID() for keys

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Poem from './react.dev/rendering-lists/challenge-04/Poem'
+import TodoList from './top/keys-in-react/TodoList'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Poem />
+    <TodoList />
   </StrictMode>,
 )
