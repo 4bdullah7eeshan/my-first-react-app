29632c9cba4b0d8ab7d22b6ed3d7968c46f971da
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 11:11:56 +0530

    feat: use list and map the items to render them

--- src/top/rendering-techniques/Animals.jsx
+++ src/top/rendering-techniques/Animals.jsx
@@ -1,16 +1,18 @@
 function Animals() {
+    const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  
     return (
       <div>
         <h1>Animals: </h1>
         <ul>
-          <li>Lion</li>
-          <li>Cow</li>
-          <li>Snake</li>
-          <li>Lizard</li>
+          {animals.map((animal) => {
+            return <li key={animal}>{animal}</li>;
+          })}
         </ul>
       </div>
     );
 }
+  
 
 export default Animals;
   
