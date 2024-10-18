8913b5c0ecfb2427ac2007126ae76421a4d5b86a
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 18:12:55 +0530

    feat: complete "avoid redundant state" section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import FeedbackForm from './react.dev/choosing-the-state-structure/FeedbackForm'
+import Form from './react.dev/choosing-the-state-structure/Form'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FeedbackForm />
+    <Form />
   </StrictMode>,
 )
