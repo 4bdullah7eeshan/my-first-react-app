d6e625599a9423f17ca7822e0c5d377f76d81b5f
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-18 15:53:51 +0530

    feat: use multiple state vars

--- src/react.dev/state-a-components-memory/Gallery.jsx
+++ src/react.dev/state-a-components-memory/Gallery.jsx
@@ -3,10 +3,14 @@ import { sculptureList } from './data.js';
 
 export default function Gallery() {
   const [index, setIndex] = useState(0);
-
+  const [showMore, setShowMore] = useState(false);
 
   function handleClick() {
     setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
@@ -22,13 +26,16 @@ export default function Gallery() {
       <h3>  
         ({index + 1} of {sculptureList.length})
       </h3>
+      <div>
+        <button onClick={handleMoreClick}>
+          {showMore ? 'Hide' : 'Show'} details
+        </button>
+      </div>
+      {showMore && <p>{sculpture.description}</p>}
       <img 
         src={sculpture.url} 
         alt={sculpture.alt}
       />
-      <p>
-        {sculpture.description}
-      </p>
     </>
   );
 }
