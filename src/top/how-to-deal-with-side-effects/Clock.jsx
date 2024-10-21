63f5c0e1e4df7e2b797e869b868f232d8ad1912a
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-21 09:43:52 +0530

    wip: wrap the setInterval fn inside useEffect
    Still goes berserk

--- src/top/how-to-deal-with-side-effects/Clock.jsx
+++ src/top/how-to-deal-with-side-effects/Clock.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Clock() {
     const [counter, setCounter] = useState(0);
 
-    setInterval(() => {
-        setCounter(count => count + 1)
-    }, 1000);
-
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(count => count + 1)
+        }, 1000);
+    })
+    
     return (
         <p>{counter} seconds have passed.</p>
     );
