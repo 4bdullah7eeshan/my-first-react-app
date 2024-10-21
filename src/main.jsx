8b241f0077aef6620c238800b9e3e23bb41b1b6e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-21 09:41:33 +0530

    wip: try to set a live timer
    This goes berserk without useEffect()

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Menu from './react.dev/choosing-the-state-structure/Menu'
+import Clock from './top/how-to-deal-with-side-effects/Clock'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Menu />
+    <Clock />
   </StrictMode>,
 )
